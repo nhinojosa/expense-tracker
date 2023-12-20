4635d95a834823a06c695df41980b2f19c6311e6
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-20 07:52:12 -0600

    calculations working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
+    <Balance :total="total"/>
+    <IncomeExpenses :income="income" :expenses="expenses" />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
 
-  export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction
-    },
-  };
+  import {ref, computed} from 'vue'
+  const transactions = ref([
+        { id: 1, text: 'Flower', amount: -19.99},
+        { id: 2, text: 'Salary', amount: 299.99},
+        { id: 3, text: 'Book', amount: -15},
+        { id: 4, text: 'Camera', amount: 150},
+  ]);
 
+  //get total
+  const total = computed(() => {
+    return transactions.value.reduce( (acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+  });
+
+  //get income
+  const income = computed(() => {
+    return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce( (acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+  });
+  //get expenses
+  const expenses = computed(() => {
+    return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce( (acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+  });
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,23 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-        <li class="plus">Paycheck <span>$400</span><button class="delete-btn">x</button></li>
+
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" 
+        v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{transaction.text}} <span>${{transaction.amount}}</span>
+            <button class="delete-btn">x</button>       
+        </li>
+        <!-- <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
+        <li class="plus">Paycheck <span>$400</span><button class="delete-btn">x</button></li> -->
     </ul>
 </template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    const props = defineProps ({
+        transactions: {
+            type: Array,
+            required: true,
+        }
+    })
+</script>
