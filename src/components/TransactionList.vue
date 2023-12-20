f4e005b4e5656fb406526e6be1a96ad279e5a740
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-20 07:10:35 -0600

    fixed list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,8 +2,6 @@
     <h3>History</h3>
     <ul id="list" class="list">
         <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-    </ul>
-    <ul id="list" class="list">
         <li class="plus">Paycheck <span>$400</span><button class="delete-btn">x</button></li>
     </ul>
 </template>
