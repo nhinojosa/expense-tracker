dcb60abadfbce5c87265adb0c8bb9c8bd9826e50
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-20 21:12:25 -0600

    Fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-company-directory/',
+  base: '/expense-tracker/',
   plugins: [
     vue(),
   ],
