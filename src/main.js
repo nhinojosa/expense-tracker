a0aa5aca11da8f79cf7b5cce0190718d167e0071
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-20 21:05:48 -0600

    Update router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css';
 
 import './assets/style.css'
 
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/expense-tracker/', name: 'Home', component: App },
+    ]
+});
+
+
 import App from './App.vue'
 const app = createApp(App)
+app.use(router);
 app.use(Toast);
 app.mount('#app')
 
